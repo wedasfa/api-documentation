8f6949f98f0bbe1faf68e27b55fadcf0e0a6ae00
fiction <me@es3n.in>
2021-05-22 13:47:27 +0200

    feat(scripts): add table migrations

---
+++ scripts/tables_migration.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from typing import List
+
+
+'''
+@note: es3n1n: this script will migrate from api classes to tables
+g_GlobalVars: -> GlobalVars.
+cheat -> Cheat
+'''
+
+root_path = Path('.')
+
+ignore = [
+	'.git',
+	'LICENSE.md',
+	'README.md',
+	'SUMMARY.md',
+	'scripts',
+]
+
+remap = {
+	'g_GlobalVars:': 'GlobalVars.',
+	'g_ClientState:': 'ClientState.',
+	'cheat.': 'Cheat.',
+	'utils': 'Utils.',
+	'menu.': 'Menu.',
+	'http.': 'Http.',
+	'ragebot.': 'RageBot.',
+	'antiaim.': 'AntiAim.',
+	'exploits.': 'Exploits.',
+	'fakelag.': 'FakeLag.',
+	'esp.': 'ESP.',
+	'g_EngineTrace:': 'EngineTrace.',
+	'g_EntityList:': 'EntityList.',
+	'g_EngineClient:': 'EngineClient.',
+	'g_DebugOverlay:': 'DebugOverlay.',
+	'g_MaterialSystem:': 'MatSystem.',  # @note: es3n1n: unsure
+	'g_MatSystem:': 'MatSystem.',
+	'g_RenderBeams:': 'RenderBeams.',
+	'g_Render:': 'Render.',
+	'g_Panorama:': 'Panorama.',
+	'g_CVar:': 'CVar.',
+}
+
+
+def _iterdir(dir: Path) -> None:
+	global result
+
+	for item in dir.iterdir():
+		if item.name in ignore:
+			continue
+
+		if item.is_dir():
+			_iterdir(item)
+			continue
+
+		with open(str(item), 'r') as f:
+			content = f.read()
+
+		for k in remap.keys():
+			content = content.replace(k, remap[k])
+
+		with open(str(item), 'w') as f:
+			f.write(content)
+
+
+if __name__ == '__main__':
+	_iterdir(root_path)
